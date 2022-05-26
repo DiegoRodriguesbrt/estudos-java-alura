71752a4bd9fa6eae7bf0786a41756a96bfbb674b
Diego Rodrigues de Brito <95620343+DiegoRodriguesbrt@users.noreply.github.com>
2022-05-26 09:07:39 -0300

    Update TestaBanco.java
    Alterando cpf

--- dbank-cliente/src/TestaBanco.java
+++ dbank-cliente/src/TestaBanco.java
@@ -3,7 +3,7 @@ public class TestaBanco {
 	public static void main(String[] args) {
 		Cliente diego = new Cliente();
 		diego.nome = "Diego Rodrigues de Brito";
-		diego.cpf = "086.919.814-90";
+		diego.cpf = "123.456.789-90";
 		diego.profissao = "programador";
 		
 		Conta contaDoDiego = new Conta();
